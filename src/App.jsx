eef852ec1b87916fcbfb60544f09abd7684e590d
FriGilix <corentinprodesign@gmail.com>
2025-02-10 14:47:41 +0100

    Création des routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
 
 
   return (
-    
+    <>
     <Routes>
-      
+      <Route path="/" element={<Home/>}/>
+      <Route path="/" element={<Destination/>}/>
+      <Route path="/" element={<Crew/>}/>
+      <Route path="/" element={<Technology/>}/>
     </Routes>
+    </>
 
   )
 }
